a0e5be964ae9d4107e64bbe67b8ec3d5d33330be
HarshT10 <harsh.thakkar1011@gmail.com>
2024-08-17 19:18:33 +0530

    fixing header bug

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -16,14 +16,14 @@ const Header = () => {
   );
 
   useEffect(() => {
-    // Function to get cookie by name
     const getCookie = (name) => {
       const value = `; ${document.cookie}`;
       const parts = value.split(`; ${name}=`);
-      if (parts.length === 2) return parts.pop().split(";").shift();
+      if (parts.length === 2) return parts.pop().split(";").shift().trim();
     };
 
     const usernameFromCookie = getCookie("username");
+    console.log("Username from cookie:", usernameFromCookie); // Debugging
     if (usernameFromCookie) {
       setUsername(usernameFromCookie);
     }
